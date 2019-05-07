e5511ab81ae34eedfaf070888537a5b115df243d
Riley McCuen <riley.mccuen@wustl.edu>
2019-05-07 13:42:17 -0500

    Full api access now set up. Not all methods tested

--- src/smite/connection/get_data.go
+++ src/smite/connection/get_data.go
@@ -3,17 +3,47 @@ package connection
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	G "smite/general"
 )
 
+type apiError struct {
+	What string
+}
+
+func (e *apiError) Error() string {
+	return fmt.Sprintf("at %s", e.What)
+}
+
 // DataHandler - handles getting fresh data from the API
 type DataHandler interface {
-	UpdateSession() *Credentials
-	TestSession() bool
-	GetItems()
-	GetGods()
+	UpdateSession() (*Credentials, error)
+	GetSession() (string, error)
+	TestSession() (bool, error)
+	GetDataUsed() (*[]byte, error)
+	GetDemoDetails() (*[]byte, error)
+	GetEsportsProLeagueDetails() (*[]byte, error)
+	GetFriends() (*[]byte, error)
+	GetGodRanks() (*[]byte, error)
+	GetGods() (*[]byte, error)
+	GetGodRecommendedItems() (*[]byte, error)
+	GetItems() (*[]byte, error)
+	GetMatchDetails() (*[]byte, error)
+	GetMatchPlayerDetails() (*[]byte, error)
+	GetMatchIdsByQueue() (*[]byte, error)
+	GetLeagueLeaderboard() (*[]byte, error)
+	GetLeagueSeasons() (*[]byte, error)
+	GetMatchHistory() (*[]byte, error)
+	GetPlayer() (*[]byte, error)
+	GetPlayerStatus() (*[]byte, error)
+	GetQueueStats() (*[]byte, error)
+	GetTeamDetails() (*[]byte, error)
+	GetTeamMatchHistory() (*[]byte, error)
+	GetTeamPlayers() (*[]byte, error)
+	GetTopMatches() (*[]byte, error)
+	SearchTeams() (*[]byte, error)
 }
 
 type apiCaller interface {
@@ -29,16 +59,18 @@ func (u *url) callAPIAndGetBody() *[]byte {
 	return &body
 }
 
-// Ping - Pings the api
-func Ping() *[]byte {
-	return buildURLStruct("ping", nil).callAPIAndGetBody()
+// Ping - A quick way of validating access to the Hi-Rez API.
+func Ping() (*[]byte, error) {
+	// /ping[ResponseFormat]
+	return buildURL("ping", nil, bMethod()).callAPIAndGetBody(), nil
 }
 
 // UpdateSession - Updates the session if it is expires and returns caller
-func (credentials *Credentials) UpdateSession() *Credentials {
+func (credentials *Credentials) UpdateSession() (*Credentials, error) {
+	// /createsession[ResponseFormat]/{developerId}/{signature}/{timestamp}
 	// Check if current session is valid
-	if !credentials.TestSession() {
-		body := buildURLStruct("createsession", credentials).callAPIAndGetBody()
+	if valid, e := credentials.TestSession(); e == nil && !valid {
+		body := buildURL("createsession", credentials, bMethod(), bCredentials(), bTimestamp()).callAPIAndGetBody()
 		// Put into struct
 		var sj sessionjson
 		json.Unmarshal(*body, &sj)
@@ -46,29 +78,230 @@ func (credentials *Credentials) UpdateSession() *Credentials {
 		credentials.SessionID = sj.SessionID
 		credentials.saveCredentials()
 	}
-	return credentials
+	return credentials, nil
 }
 
 // GetSession - Gets a new session and puts it in credentials
-func (credentials *Credentials) GetSession() string {
-	return credentials.UpdateSession().SessionID
+func (credentials *Credentials) GetSession() (string, error) {
+	ret, e := credentials.UpdateSession()
+	return ret.SessionID, e
 }
 
 // TestSession - Tests that the session is valid, returns true if session is valid, false if expired
-func (credentials *Credentials) TestSession() bool {
-	body := buildURLStruct("testsession", credentials).callAPIAndGetBody()
+func (credentials *Credentials) TestSession() (bool, error) {
+	// /testsession[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}
+	body := buildURL("testsession", credentials, bMethod(), bCredentials(), bSession(), bTimestamp()).callAPIAndGetBody()
 	// If response body contains "Invalid" then session has expired
-	return !bytes.Contains(*body, []byte("Invalid"))
+	return !bytes.Contains(*body, []byte("Invalid")), nil
 }
 
-// GetItems - gets all current item data and puts it in json file
-func (credentials *Credentials) GetItems() {
-	body := buildURLStruct("getitems", credentials).callAPIAndGetBody()
-	ioutil.WriteFile(G.ItemsPath, *body, 777)
+// GetDataUsed - returns API Developer daily usage limits and the current status against those limits.
+func (credentials *Credentials) GetDataUsed() (*[]byte, error) {
+	// /getdataused[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}
+	body := buildURL("getdataused", credentials, bMethod(), bCredentials(), bSession(), bTimestamp()).callAPIAndGetBody()
+	fmt.Println(string(*body))
+	return body, nil
+}
+
+// GetDemoDetails - returns information regarding a particular match. Rarely used in lieu of getmatchdetails().
+func (credentials *Credentials) GetDemoDetails(matchID int) (*[]byte, error) {
+	// /getdemodetails[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}/{match_id}
+	body := buildURL("getdemodetails", credentials, bMethod(), bCredentials(), bSession(), bTimestamp(), bAny(matchID)).callAPIAndGetBody()
+	fmt.Println(string(*body))
+	return body, nil
+}
+
+// GetEsportsProLeagueDetails - Returns the matchup information for each matchup for the current eSports Pro League season. An important return value is
+// “match_status” which represents a match being scheduled (1), in-progress (2), or complete (3)
+func (credentials *Credentials) GetEsportsProLeagueDetails() (*[]byte, error) {
+	// /getesportsproleaguedetails[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}
+	body := buildURL("getesportsproleaguedetails", credentials, bMethod(), bCredentials(), bSession(), bTimestamp()).callAPIAndGetBody()
+	fmt.Println(string(*body))
+	return body, nil
+}
+
+// GetFriends - returns the Smite User names of each of the player’s friends.
+func (credentials *Credentials) GetFriends(player interface{}) (*[]byte, error) {
+	// /getfriends[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}/{player}
+	var body *[]byte
+	switch player.(type) {
+	case string, int:
+		body = buildURL("getfriends", credentials, bMethod(), bCredentials(), bSession(), bTimestamp(), bAny(player)).callAPIAndGetBody()
+	default:
+		return nil, &apiError{"player input is of wrong type. Should be of string or int."}
+	}
+	fmt.Println(string(*body))
+	return body, nil
+}
+
+// GetGodRanks - returns the Rank and Worshippers value for each God a player has played.
+func (credentials *Credentials) GetGodRanks(player interface{}) (*[]byte, error) {
+	// /getgodranks[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}/{player}
+	var body *[]byte
+	switch player.(type) {
+	case string, int:
+		body = buildURL("getgodranks", credentials, bMethod(), bCredentials(), bSession(), bTimestamp(), bAny(player)).callAPIAndGetBody()
+	default:
+		return nil, &apiError{"player input is of wrong type. Should be of string or int."}
+	}
+	fmt.Println(string(*body))
+	return body, nil
 }
 
 // GetGods - gets all current god data and puts it in json file
-func (credentials *Credentials) GetGods() {
-	body := buildURLStruct("getgods", credentials).callAPIAndGetBody()
+func (credentials *Credentials) GetGods() (*[]byte, error) {
+	// /getgods[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}/{languageCode}
+	body := buildURL("getgods", credentials, bMethod(), bCredentials(), bSession(), bTimestamp(), bLanguageCode()).callAPIAndGetBody()
 	ioutil.WriteFile(G.GodsPath, *body, 777)
+	return body, nil
+}
+
+// GetGodRecommendedItems - returns the Recommended Items for a particular God.
+func (credentials *Credentials) GetGodRecommendedItems(godID int) (*[]byte, error) {
+	// /getgodrecommendeditems[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}/{godid}/{languageCode}
+	body := buildURL("getgodrecommendeditems", credentials, bMethod(), bCredentials(), bSession(), bTimestamp(), bAny(godID), bLanguageCode()).callAPIAndGetBody()
+	fmt.Println(string(*body))
+	return body, nil
+}
+
+// GetItems - gets all current item data and puts it in json file
+func (credentials *Credentials) GetItems() (*[]byte, error) {
+	// /getitems[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}/{languagecode}
+	body := buildURL("getitems", credentials, bMethod(), bCredentials(), bSession(), bTimestamp(), bLanguageCode()).callAPIAndGetBody()
+	ioutil.WriteFile(G.ItemsPath, *body, 777)
+	return body, nil
+}
+
+// GetMatchDetails - returns the statistics for a particular completed match.
+func (credentials *Credentials) GetMatchDetails() (*[]byte, error) {
+	// /getmatchdetails[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}/{match_id}
+	body := buildURL("getmatchdetails", credentials, bMethod(), bCredentials(), bSession(), bTimestamp()).callAPIAndGetBody()
+	fmt.Println(string(*body))
+	return body, nil
+}
+
+// GetMatchPlayerDetails - returns player information for a live match.
+func (credentials *Credentials) GetMatchPlayerDetails(matchID int) (*[]byte, error) {
+	// /getmatchplayerdetails[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}/{match_id}
+	body := buildURL("getmatchplayerdetails", credentials, bMethod(), bCredentials(), bSession(), bTimestamp(), bAny(matchID)).callAPIAndGetBody()
+	fmt.Println(string(*body))
+	return body, nil
+}
+
+// GetMatchIdsByQueue - lists all Match IDs for a particular Match Queue; useful for API developers interested in constructing data by Queue. To
+// limit the data returned, an {hour} parameter was added (valid values: 0 - 23). An {hour} parameter of -1 represents the
+// entire day, but be warned that this may be more data than we can return for certain queues. Also, a returned “active_flag”
+// means that there is no match information/stats for the corresponding match. Usually due to a match being in-progress,
+// though there could be other reasons.
+func (credentials *Credentials) GetMatchIdsByQueue(q queue, date, hour string) (*[]byte, error) {
+	// /getmatchidsbyqueue[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}/{queue}/{date}/{hour}
+	body := buildURL("getmatchidsbyqueue", credentials, bMethod(), bCredentials(), bSession(), bTimestamp(), bAny(q), bAny(date), bAny(hour)).callAPIAndGetBody()
+	fmt.Println(string(*body))
+	return body, nil
+}
+
+// GetLeagueLeaderboard - returns the top players for a particular league (as indicated by the queue/tier/season parameters).
+func (credentials *Credentials) GetLeagueLeaderboard(q queue, t tier, season string) (*[]byte, error) {
+	// /getleagueleaderboard[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}/{queue}/{tier}/{season}
+	body := buildURL("getleagueleaderboard", credentials, bMethod(), bCredentials(), bSession(), bTimestamp(), bAny(q), bAny(t), bAny(season)).callAPIAndGetBody()
+	fmt.Println(string(*body))
+	return body, nil
+}
+
+// GetLeagueSeasons - provides a list of seasons (including the single active season) for a match queue.
+func (credentials *Credentials) GetLeagueSeasons(q queue) (*[]byte, error) {
+	// /getleagueseasons[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}/{queue}
+	body := buildURL("getleagueseasons", credentials, bMethod(), bCredentials(), bSession(), bTimestamp(), bAny(q)).callAPIAndGetBody()
+	fmt.Println(string(*body))
+	return body, nil
+}
+
+// GetMatchHistory - gets recent matches and high level match statistics for a particular player.
+func (credentials *Credentials) GetMatchHistory(player interface{}) (*[]byte, error) {
+	// /getmatchhistory[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}/{player}
+	var body *[]byte
+	switch player.(type) {
+	case string, int:
+		body = buildURL("getmatchhistory", credentials, bMethod(), bCredentials(), bSession(), bTimestamp(), bAny(player)).callAPIAndGetBody()
+	default:
+		return nil, &apiError{"player input is of wrong type. Should be of string or int."}
+	}
+	fmt.Println(string(*body))
+	return body, nil
+}
+
+// GetPlayer - returns league and other high level data for a particular player.
+func (credentials *Credentials) GetPlayer(playerName string) (*[]byte, error) {
+	// /getplayer[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}/{playerName}
+	body := buildURL("getplayer", credentials, bMethod(), bCredentials(), bSession(), bTimestamp(), bAny(playerName)).callAPIAndGetBody()
+	fmt.Println(string(*body))
+	return body, nil
+}
+
+// GetPlayerStatus - returns player status as follows:
+func (credentials *Credentials) GetPlayerStatus(player interface{}) (*[]byte, error) {
+	// /getplayerstatus[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}/{player}
+	var body *[]byte
+	switch player.(type) {
+	case string, int:
+		body = buildURL("getplayerstatus", credentials, bMethod(), bCredentials(), bSession(), bTimestamp(), bAny(player)).callAPIAndGetBody()
+	default:
+		return nil, &apiError{"player input is of wrong type. Should be of string or int."}
+	}
+	fmt.Println(string(*body))
+	return body, nil
+}
+
+// GetQueueStats - returns match summary statistics for a (player, queue) combination grouped by gods played.
+func (credentials *Credentials) GetQueueStats(player interface{}, q queue) (*[]byte, error) {
+	// /getqueuestats[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}/{player}/{queue}
+	var body *[]byte
+	switch player.(type) {
+	case string, int:
+		body = buildURL("getqueuestats", credentials, bMethod(), bCredentials(), bSession(), bTimestamp(), bAny(player), bAny(q)).callAPIAndGetBody()
+	default:
+		return nil, &apiError{"player input is of wrong type. Should be of string or int."}
+	}
+	fmt.Println(string(*body))
+	return body, nil
+}
+
+// GetTeamDetails - lists the number of players and other high level details for a particular clan.
+func (credentials *Credentials) GetTeamDetails(clanID string) (*[]byte, error) {
+	// /getteamdetails[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}/{clanid}
+	body := buildURL("getteamdetails", credentials, bMethod(), bCredentials(), bSession(), bTimestamp(), bAny(clanID)).callAPIAndGetBody()
+	fmt.Println(string(*body))
+	return body, nil
+}
+
+// GetTeamMatchHistory - gets recent matches and high level match statistics for a particular clan/team.
+func (credentials *Credentials) GetTeamMatchHistory(clanID string) (*[]byte, error) {
+	// /getteammatchhistory[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}/{clanid}
+	body := buildURL("getteammatchhistory", credentials, bMethod(), bCredentials(), bSession(), bTimestamp(), bAny(clanID)).callAPIAndGetBody()
+	fmt.Println(string(*body))
+	return body, nil
+}
+
+// GetTeamPlayers - lists the players for a particular clan.
+func (credentials *Credentials) GetTeamPlayers(clanID string) (*[]byte, error) {
+	// /getteamplayers[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}/{clanid}
+	body := buildURL("getteamplayers", credentials, bMethod(), bCredentials(), bSession(), bTimestamp(), bAny(clanID)).callAPIAndGetBody()
+	fmt.Println(string(*body))
+	return body, nil
+}
+
+// GetTopMatches - lists the 50 most watched / most recent recorded matches.
+func (credentials *Credentials) GetTopMatches() (*[]byte, error) {
+	// /gettopmatches[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}
+	body := buildURL("gettopmatches", credentials, bMethod(), bCredentials(), bSession(), bTimestamp()).callAPIAndGetBody()
+	fmt.Println(string(*body))
+	return body, nil
+}
+
+// SearchTeams - returns high level information for Team names containing the “searchTeam” string.
+func (credentials *Credentials) SearchTeams(searchTeam string) (*[]byte, error) {
+	// /searchteams[ResponseFormat]/{developerId}/{signature}/{session}/{timestamp}/{searchTeam}
+	body := buildURL("searchteams", credentials, bMethod(), bCredentials(), bSession(), bTimestamp(), bAny(searchTeam)).callAPIAndGetBody()
+	fmt.Println(string(*body))
+	return body, nil
 }
--- src/smite/connection/urls.go
+++ src/smite/connection/urls.go
@@ -2,7 +2,9 @@ package connection
 
 import (
 	"fmt"
+	"regexp"
 	G "smite/general"
+	"strings"
 )
 
 type url struct {
@@ -31,29 +33,104 @@ func newURL(method string, credentials *Credentials) *url {
 	return ret
 }
 
+// the status of a player
+type playerstatus int
+
+const (
+	//  Offline
+	offline playerstatus = 0
+	// In Lobby (basically anywhere except god selection or in game)
+	inlobby playerstatus = 1
+	// God Selection (player has accepted match and is selecting god before start of game)
+	godselection playerstatus = 2
+	// In Game (match has started)
+	ingame playerstatus = 3
+	// Online (player is logged in, but may be blocking broadcast of player state)
+	online playerstatus = 4
+)
+
+// the id of the game mode
+type queue int
+
+// all queues in SMITE
+const (
+	Conquest5v5         queue = 423
+	NoviceQueue         queue = 424
+	Conquest            queue = 426
+	Practice            queue = 427
+	ConquestChallenge   queue = 429
+	ConquestRanked      queue = 430
+	Domination          queue = 433
+	MOTD434             queue = 434 //(use with 465 to get all MOTD matches)
+	Arena               queue = 435
+	ArenaChallenge      queue = 438
+	DominationChallenge queue = 439
+	JoustLeague         queue = 440
+	JoustChallenge      queue = 441
+	Assault             queue = 445
+	AssaultChallenge    queue = 446
+	Joust3v3            queue = 448
+	ConquestLeague      queue = 451
+	ArenaLeague         queue = 452
+	MOTD465             queue = 465 //(Supports “closing” the Queue by our platform; use with 434)
+)
+
+// the tier of the match
+type tier int
+
+// all tiers in SMITE
+const (
+	BronzeV     tier = 1
+	BronzeIV    tier = 2
+	BronzeIII   tier = 3
+	BronzeII    tier = 4
+	BronzeI     tier = 5
+	SilverV     tier = 6
+	SilverIV    tier = 7
+	SilverIII   tier = 8
+	SilverII    tier = 9
+	SilverI     tier = 10
+	GoldV       tier = 11
+	GoldIV      tier = 12
+	GoldIII     tier = 13
+	GoldII      tier = 14
+	GoldI       tier = 15
+	PlatinumV   tier = 16
+	PlatinumIV  tier = 17
+	PlatinumIII tier = 18
+	PlatinumII  tier = 19
+	PlatinumI   tier = 20
+	DiamondV    tier = 21
+	DiamondIV   tier = 22
+	DiamondIII  tier = 23
+	DiamondII   tier = 24
+	DiamondI    tier = 25
+	MastersI    tier = 26
+)
+
 // bmethod - returns a function that adds the method related info to a url
-func bmethod() func(u *url) {
+func bMethod() func(u *url) {
 	return func(u *url) {
 		u.sURL = fmt.Sprintf("%s/%s%s", u.sURL, u.method, G.APIResponseFormat.String())
 	}
 }
 
 // bcredentials - returns a function that adds the credentials related info to a url
-func bcredentials() func(u *url) {
+func bCredentials() func(u *url) {
 	return func(u *url) {
 		u.sURL = fmt.Sprintf("%s/%s/%s", u.sURL, u.credentials.DevID, u.credentials.GetSignature(u.method, u.timestamp))
 	}
 }
 
 // bsession - returns a function that adds the session related info to a url
-func bsession() func(u *url) {
+func bSession() func(u *url) {
 	return func(u *url) {
 		u.sURL = fmt.Sprintf("%s/%s", u.sURL, u.credentials.SessionID)
 	}
 }
 
 // btimestamp - returns a function that adds the timestamp to a url
-func btimestamp() func(u *url) {
+func bTimestamp() func(u *url) {
 	return func(u *url) {
 		u.sURL = fmt.Sprintf("%s/%s", u.sURL, u.timestamp)
 	}
@@ -66,6 +143,13 @@ func bLanguageCode() func(u *url) {
 	}
 }
 
+// bAny - returns a function that adds any arguments to the end of the url
+func bAny(any interface{}) func(u *url) {
+	return func(u *url) {
+		u.sURL = fmt.Sprintf("%s/%s", u.sURL, fmt.Sprint(any))
+	}
+}
+
 // buildURL - builds a url given the necessary data
 func buildURL(method string, credentials *Credentials, builders ...func(u *url)) *url {
 	ret := newURL(method, credentials)
@@ -75,25 +159,48 @@ func buildURL(method string, credentials *Credentials, builders ...func(u *url))
 	return ret
 }
 
-// buildURLStruct - builds the url given the necessary data and returns a struct
-func buildURLStruct(method string, credentials *Credentials) *url {
-	switch method {
-	case "ping":
-		return buildURL("ping", nil, bmethod())
-	case "createsession":
-		return buildURL("createsession", credentials, bmethod(), bcredentials(), btimestamp())
-	case "testsession":
-		return buildURL("testsession", credentials, bmethod(), bcredentials(), bsession(), btimestamp())
-	case "getitems":
-		return buildURL("getitems", credentials, bmethod(), bcredentials(), bsession(), btimestamp(), bLanguageCode())
-	case "getgods":
-		return buildURL("getgods", credentials, bmethod(), bcredentials(), bsession(), btimestamp(), bLanguageCode())
-	default:
-		return nil
+// BuildAPIMethod - small function that I helped to use to parse api documentation to make functions quicker
+func BuildAPIMethod(name, apicall string) {
+	var sbuilder strings.Builder
+	sbuilder.WriteString(`buildURL(`)
+	method, _ := regexp.Compile("([a-z]+)(?:\\[ResponseFormat\\])")
+	methodLowercaseName := method.FindStringSubmatch(apicall)[1]
+	sbuilder.WriteString(fmt.Sprintf(`"%s", credentials, bMethod(), `, methodLowercaseName))
+	build, _ := regexp.Compile("(?:\\{)([a-zA-Z]*)(?:\\})")
+	matches := build.FindAllStringSubmatch(apicall, -1)
+	for _, match := range matches {
+		m := match[1]
+		switch m {
+		case "developerId":
+			sbuilder.WriteString(fmt.Sprintf(`%s, `, "bCredentials()"))
+			break
+		case "signature":
+			break
+		case "session":
+			sbuilder.WriteString(fmt.Sprintf(`%s, `, "bSession()"))
+			break
+		case "timestamp":
+			sbuilder.WriteString(fmt.Sprintf(`%s, `, "bTimestamp()"))
+			break
+		case "languageCode":
+			sbuilder.WriteString(fmt.Sprintf(`%s, `, "bLanguageCode()"))
+			break
+		default:
+			sbuilder.WriteString(fmt.Sprintf(`bAny(%s), `, m))
+			break
+		}
 	}
-}
-
-// BuildURLString - builds a url given the necessary data
-func BuildURLString(method string, credentials *Credentials) string {
-	return buildURLStruct(method, credentials).sURL
+	sl := sbuilder.String()[:sbuilder.Len()-2]
+	var finb strings.Builder
+	finb.WriteString(sl)
+	finb.WriteString(")")
+	fmt.Printf(
+		`
+		func (credentials *Credentials) %s() (*[]byte, error) {
+			// %s
+			body := %s.callAPIAndGetBody()
+			fmt.Println(string(*body))
+			return body, nil
+		}
+		`, name, apicall, finb.String())
 }
